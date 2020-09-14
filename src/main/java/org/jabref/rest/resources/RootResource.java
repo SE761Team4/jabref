083b48d2e0cac2cafd228691dd7a94d7b7202c37
Rupert Moran-Adcock <rupert.moran.adcock@gmail.com>
2020-09-15 11:02:51 +1200

    Add GET method for maps

--- src/main/java/org/jabref/rest/resources/RootResource.java
+++ src/main/java/org/jabref/rest/resources/RootResource.java
@@ -2,10 +2,12 @@ package org.jabref.rest.resources;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+
+import org.jabref.logic.jabmap.BibTeXMindMapAdapter;
 import org.jabref.logic.jabmap.MindMapManager;
 import org.jabref.model.entry.BibEntry;
 import org.jabref.model.entry.BibEntrySerializer;
-import org.jabref.model.entry.field.Field;
+import org.jabref.model.jabmap.MindMap;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -22,7 +24,21 @@ public class RootResource {
     public String getEntries() {
         List<BibEntry> entries = MindMapManager.instance().getBibEntries();
         Gson gson = new GsonBuilder().registerTypeAdapter(BibEntry.class, new BibEntrySerializer()).create();
-        return gson.toJson(entries.get(0));
+        return gson.toJson(entries);
+    }
+
+    @GET
+    @Path("map")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getMap() {
+        List<BibEntry> entries = MindMapManager.instance().getMapEntries();
+
+        MindMap map = BibTeXMindMapAdapter.instance().bibTeX2MindMap(entries);
+
+        Gson gson = new Gson();
+//        Gson gson = new GsonBuilder().registerTypeAdapter(BibEntry.class, new BibEntrySerializer()).create();
+//        return gson.toJson(entries.get(0));
+        return gson.toJson(map);
     }
 }
 
