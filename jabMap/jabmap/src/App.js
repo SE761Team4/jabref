5a8dab06ee337980d7cb541115d209db78c51bea
isabellelucy <izzbee123@gmail.com>
2020-09-14 17:45:37 +1200

    Added toolbar/ sidebar edits

--- jabMap/jabmap/src/App.js
+++ jabMap/jabmap/src/App.js
@@ -2,14 +2,18 @@ import React from 'react';
 //import logo from './logo.svg';
 import './App.css';
 import { ReferenceList } from './components/sidebar.jsx'; 
-import Toolbar from '@material-ui/core/Toolbar';
+import { Editor } from './components/toolbar.jsx';
 
 function App() {
   return (
     <div className="App">
-		<div className="Reference-proportions">
-			<ReferenceList /> 
+		<div className = "App-header">
+			<div className="Reference-proportions">
+				<ReferenceList /> 			
+			</div>
+			<Editor/>
 		</div>
+		
     </div>
 	
   );
--- jabMap/jabmap/src/components/sidebar.jsx
+++ jabMap/jabmap/src/components/sidebar.jsx
@@ -1,36 +1,41 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
+import Table from 'react-bootstrap/Table'
 import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
+import './sidebar.css';
 
-
-
+//class='overflow-hidden'
 
 export const ReferenceList = (props) => {
-
   return (
-    <TableContainer component={Paper}>
-      <Table aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell align="left">References</TableCell>
-          </TableRow>
-		  <TableRow>
-            <TableCell align="left" >Author</TableCell>
-			<TableCell align="left" >Title</TableCell>
-			<TableCell align="left">Year</TableCell>
-			<TableCell align="left">Journal</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-         
-          
-        </TableBody>
+    <TableContainer component={Paper} className="Table-proportions" >
+      <Table className = "table table-bordered table-striped table-hover" aria-label="simple table" >
+        <thead>
+          <tr>
+           References
+          </tr>
+		  <tr>
+            <td className = "Table-proportions" >Author</td>
+			<td  >Title</td>
+			<td >Year</td>
+			<td >Journal</td>
+          </tr>
+        </thead>
+        <tbody>
+			<tr>
+            <td  >Name</td>
+			<td >A paper with title</td>
+			<td >2010</td>
+			<td >Science things</td>
+          </tr>
+          <tr>
+            <td  >Name</td>
+			<td >A paper </td>
+			<td >2010</td>
+			<td >Science</td>
+          </tr>
+        </tbody>
       </Table>
     </TableContainer>
   );
