085460996c0d00cae4ec4cbb10e17b89b4cd2e4e
Rupert Moran-Adcock <rupert.moran.adcock@gmail.com>
2020-09-25 12:14:37 +1200

    Added styling to refs table
    Using Material UI components for ease of styling and clean look and feel.

--- jabMap/jabmap/src/App.js
+++ jabMap/jabmap/src/App.js
@@ -1,29 +1,52 @@
 import React, { Component } from "react";
 //import logo from './logo.svg';
 import "./App.css";
-import References from "./components/References";
+import ReferenceList from "./components/sidebar";
 import { Editor } from "./components/toolbar.jsx";
 
 class App extends Component {
   state = {
-    references: [],
+    references: [
+      {
+        author: "An author",
+        title: "A paper",
+        year: 2010,
+      },
+      {
+        author: "The author",
+        title: "The paper",
+        year: 2011,
+      },
+      {
+        author: "Author 1",
+        title: "A book",
+        year: 2010,
+      },
+      {
+        author: "Famous author",
+        title: "An article",
+        year: 2012,
+      }
+    ],
   };
 
-  componentDidMount() {
-    fetch("/libraries/current/entries")
-      .then((res) => res.json())
-      .then((data) => {
-        this.setState({ references: data });
-      })
-      .catch(console.log);
-  }
+  // componentDidMount() {
+  //   fetch("/libraries/current/entries")
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       this.setState({ references: data });
+  //     })
+  //     .catch(console.log);
+  // }
+
+
 
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <div className="Reference-proportions">
-            {this.state.references && <References references={this.state.references} />}
+            {this.state.references && <ReferenceList references={this.state.references} />}
           </div>
           <Editor />
         </div>
--- jabMap/jabmap/src/components/sidebar.jsx
+++ jabMap/jabmap/src/components/sidebar.jsx
@@ -1,38 +1,76 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Table from 'react-bootstrap/Table'
+import React, { useState } from 'react';
+import { withStyles, makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import './sidebar.css';
 
-//class='overflow-hidden'
+const StyledTableCell = withStyles((theme) => ({
+  head: {
+    fontWeight: 'bold',
+    fontSize: 12,
+  },
+  body: {
+    fontSize: 12,
+    fontFamily: 'Arial',
+    border: 'none',
+  },
+}))(TableCell);
 
-export const ReferenceList = (references) => {
+const StyledTableRow = withStyles((theme) => ({
+  root: {
+    height: 25,
+    '&:nth-of-type(even)': {
+      backgroundColor: '#efefef',
+    },
+  },
+}))(TableRow);
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 100,
+  },
+});
+
+const ReferenceList = ({references}) => {
+  const classes = useStyles();
+  const [numRows, setNumRows] = useState(10);
+
   return (
-    <TableContainer component={Paper} className="Table-proportions" >
-      <Table className = "table table-bordered table-striped table-hover" aria-label="simple table" >
-        <thead>
-          <tr>
-           References
-          </tr>
-		  <tr>
-            <td className = "Table-proportions" >Author</td>
-			<td  >Title</td>
-			<td >Year</td>
-			<td >Journal</td>
-          </tr>
-        </thead>
-		{/* Add references.map here when API data is ready */}
-        <tbody>
-			<tr>
-            <td  >Name</td>
-			<td >A paper with title</td>
-			<td >2010</td>
-			<td >Science things</td>
-          </tr>
-        </tbody>
+    <TableContainer component={Paper}>
+      <Table className={classes.table} size="small" aria-label="refs table">
+        <TableHead>
+          <TableRow>
+            <StyledTableCell>Author</StyledTableCell>
+            <StyledTableCell>Title</StyledTableCell>
+            <StyledTableCell>Year</StyledTableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+        {references &&
+          references.map((ref) => (
+            <StyledTableRow key={ref.title}>
+              <StyledTableCell component="th" scope="row">{ref.title}</StyledTableCell>
+              <StyledTableCell>{ref.author}</StyledTableCell>
+              <StyledTableCell>{ref.year}</StyledTableCell>
+            </StyledTableRow>
+        ))}
+        {Array(numRows - references.length).fill(
+          <StyledTableRow>
+            <StyledTableCell></StyledTableCell>
+            <StyledTableCell></StyledTableCell>
+            <StyledTableCell></StyledTableCell>
+          </StyledTableRow>
+        )}
+        </TableBody>
       </Table>
     </TableContainer>
   );
 }
+
+export default ReferenceList;
 
